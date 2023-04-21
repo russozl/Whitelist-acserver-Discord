4405c7876681e426fe6ad7f748306fa0266af853
russozl <102490193+russozl@users.noreply.github.com>
2023-04-21 01:52:37 -0300

    Update whitelist.py

--- whitelist.py
+++ whitelist.py
@@ -10,19 +10,19 @@ from discord.ext import commands
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='!', intents=intents)
 
-# Define o caminho do arquivo de texto
-path = r'C:\Users\sergi\Desktop\saitama bot\whitelist.txt'
+# Sets the path of the text file
+path = r'C:\Users\youruser\whitelist.txt'
 
-# Define o ID do cargo que pode remover o código do usuário
+# Sets the ID of the role that can remove the user's code
 role_id = 1075880077641912451
 
-# Carrega a lista de códigos de whitelist do arquivo whitelist.txt
+# Loads the whitelist codes from the whitelist.txt file
 def load_whitelist():
     with open(path, 'r') as f:
         codes = f.read().splitlines()
     return codes
 
-# Salva a lista de códigos no arquivo whitelist.txt
+# Saves the list of codes to the whitelist.txt file
 def save_whitelist(codes):
     with open(path, 'w') as f:
         for code in codes:
@@ -30,9 +30,9 @@ def save_whitelist(codes):
 
 @client.event
 async def on_member_update(before, after):
-    # Verifica se o usuário perdeu o cargo especificado
+    # Checks if the user lost the specified role
     if any(role.id == role_id for role in before.roles) and not any(role.id == role_id for role in after.roles):
-        # Remove o código do usuário da lista de códigos de whitelist
+        # Removes the user's code from the whitelist codes list
         codes = load_whitelist()
         user_code = str(after.id)
         if user_code in codes:
@@ -42,34 +42,34 @@ async def on_member_update(before, after):
                 codes.remove(code)
                 codes.remove(user_code)
                 save_whitelist(codes)
-                print(f"Código {code} removido da lista de whitelist.")
+                print(f"Code {code} removed from the whitelist.")
 
 @client.command()
 async def whitelist(ctx, code: str = None):
     if code is None:
-        # Envia a mensagem no canal de "whitelist"
-        await ctx.send(f"{ctx.author.mention}, por favor, forneça um código de 17 dígitos encontrado na sua conta Steam e envie-o no canal de registro de whitelist.")
+        # Sends a message in the "whitelist" channel
+        await ctx.send(f"{ctx.author.mention}, please provide a 17-digit code found on your Steam account and send it in the whitelist registration channel.")
     elif len(code) != 17 or not code.isdigit():
-        # Envia a mensagem no canal de "whitelist"
-        await ctx.send(f"{ctx.author.mention}, código inválido! Verifique se o código tem 17 dígitos e é composto apenas de números. O código pode ser encontrado na sua conta Steam.")
+        # Sends a message in the "whitelist" channel
+        await ctx.send(f"{ctx.author.mention}, invalid code! Make sure the code is 17 digits long and consists only of numbers. The code can be found on your Steam account.")
     else:
-        # Carrega a lista de códigos de whitelist do arquivo whitelist.txt
+        # Loads the whitelist codes from the whitelist.txt file
         codes = load_whitelist()
 
-        # Verifica se o usuário já adicionou um código à lista
+        # Checks if the user has already added a code to the list
         user_code = str(ctx.author.id)
         if user_code in codes:
-            # Envia a mensagem no canal de "whitelist"
-            await ctx.send(f"{ctx.author.mention}, você já adicionou um código à lista. Se precisar adicionar outro código, entre em contato com um administrador.")
+            # Sends a message in the "whitelist" channel
+            await ctx.send(f"{ctx.author.mention}, you have already added a code to the list. If you need to add another code, please contact an administrator.")
         elif code in codes:
-            # Envia a mensagem no canal de "whitelist"
-            await ctx.send(f"{ctx.author.mention}, esse código de whitelist já foi adicionado à lista. Se precisar adicionar outro código, entre em contato com um administrador.")
+            # Sends a message in the "whitelist" channel
+            await ctx.send(f"{ctx.author.mention}, this whitelist code has already been added to the list. If you need to add another code, please contact an administrator.")
         else:
-            # Adiciona o novo código à lista de códigos de whitelist
+            # Adds the new code to the whitelist codes list
             codes.append(code)
-            # Adiciona o ID do usuário à lista para verificar se ele já adicionou um código
+            # Adds the user ID to the list to check if they have already added a code
             codes.append(user_code)
-            # Salva a lista atualizada no arquivo whitelist.txt
+            # Saves the updated list to the whitelist.txt file
             save_whitelist(codes)
 
             # Adiciona a role "passed" ao usuário
@@ -77,6 +77,6 @@ async def whitelist(ctx, code: str = None):
             role = discord.utils.get(user.guild.roles, name="passed")
             await user.add_roles(role)
 
-            # Envia a mensagem no canal de "whitelist"
-            await ctx.send(f"{ctx.author.mention}, whitelist liberada com sucesso! Por favor, siga para o canal de download para concluir a instalação.")
+            # Sends a message in the "whitelist" channel
+            await ctx.send(f"{ctx.author.mention}, whitelist successfully released! Please proceed to the download channel to complete the installation.")
 
